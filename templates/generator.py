a94af4892cbb2d317a68a1c34279eb74cd445a98
Will Hohyon Ryu <will@oxopolitics.com>
2026-03-25 21:42:52 +0900

    security: 개인정보 포함 파일 전부 삭제

--- templates/generator.py
+++ templates/generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-HWPX 문서 생성기 — 최소 작동 예제
-사용법: python generator.py output.hwpx "문서 제목" "본문 내용"
+HWPX 문서 생성기
+사용법: python generator.py output.hwpx "문서 제목" "본문 내용" [A4|B4] [1|2]
 """
 import zipfile
+import html
 import sys
 from pathlib import Path
 
@@ -14,140 +15,69 @@ def read_template(name: str) -> str:
     return (TEMPLATES / name).read_text(encoding="utf-8")
 
 
-HEADER_XML = """<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<hh:head xmlns:hh="http://www.hancom.co.kr/hwpml/2011/head"
-         xmlns:hc="http://www.hancom.co.kr/hwpml/2011/core"
-         xmlns:hp="http://www.hancom.co.kr/hwpml/2011/paragraph"
-         version="1.5" secCnt="1">
-  <hh:beginNum page="1" footnote="1" endnote="1" pic="1" tbl="1" equation="1"/>
-  <hh:refList>
-    <hh:fontfaces itemCnt="1">
-      <hh:fontface lang="HANGUL" fontCnt="1">
-        <hh:font id="0" face="\\ub098\\ub214\\uace0\\ub515" type="TTF" isEmbedded="0"/>
-      </hh:fontface>
-    </hh:fontfaces>
-    <hh:borderFills itemCnt="1">
-      <hh:borderFill id="1" threeD="0" shadow="0" centerLine="NONE" breakCellSeparateLine="0">
-        <hh:slash type="NONE" Crooked="0" isCounter="0"/>
-        <hh:backSlash type="NONE" Crooked="0" isCounter="0"/>
-        <hh:leftBorder type="NONE" width="0.1 mm" color="#000000"/>
-        <hh:rightBorder type="NONE" width="0.1 mm" color="#000000"/>
-        <hh:topBorder type="NONE" width="0.1 mm" color="#000000"/>
-        <hh:bottomBorder type="NONE" width="0.1 mm" color="#000000"/>
-        <hh:diagonal type="SOLID" width="0.1 mm" color="#000000"/>
-      </hh:borderFill>
-    </hh:borderFills>
-    <hh:charProperties itemCnt="1">
-      <hh:charPr id="0" height="1000" textColor="#000000" shadeColor="none"
-                 useFontSpace="0" useKerning="0" symMark="NONE" borderFillIDRef="1">
-        <hh:fontRef hangul="0" latin="0" hanja="0" japanese="0" other="0" symbol="0" user="0"/>
-        <hh:ratio hangul="100" latin="100" hanja="100" japanese="100" other="100" symbol="100" user="100"/>
-        <hh:spacing hangul="0" latin="0" hanja="0" japanese="0" other="0" symbol="0" user="0"/>
-        <hh:relSz hangul="100" latin="100" hanja="100" japanese="100" other="100" symbol="100" user="100"/>
-        <hh:offset hangul="0" latin="0" hanja="0" japanese="0" other="0" symbol="0" user="0"/>
-        <hh:underline type="NONE" shape="SOLID" color="#000000"/>
-        <hh:strikeout shape="NONE" color="#000000"/>
-        <hh:outline type="NONE"/>
-        <hh:shadow type="NONE" color="#C0C0C0" offsetX="10" offsetY="10"/>
-      </hh:charPr>
-    </hh:charProperties>
-    <hh:tabProperties itemCnt="1">
-      <hh:tabPr id="0" autoTabLeft="0" autoTabRight="0"/>
-    </hh:tabProperties>
-    <hh:numberings itemCnt="0"/>
-    <hh:bullets itemCnt="0"/>
-    <hh:paraProperties itemCnt="1">
-      <hh:paraPr id="0" tabPrIDRef="0" condense="0" fontLineHeight="0"
-                 snapToGrid="1" suppressLineNumbers="0" checked="0">
-        <hh:align horizontal="LEFT" vertical="BASELINE"/>
-        <hh:heading type="NONE" idRef="0" level="0"/>
-        <hh:breakSetting breakLatinWord="KEEP_WORD" breakNonLatinWord="KEEP_WORD"
-                         widowOrphan="0" keepWithNext="0" keepLines="0"
-                         pageBreakBefore="0" lineWrap="BREAK"/>
-        <hh:autoSpacing eAsianEng="0" eAsianNum="0"/>
-      </hh:paraPr>
-    </hh:paraProperties>
-    <hh:styles itemCnt="1">
-      <hh:style id="0" type="PARA" name="\\ubc14\\ud0d5\\uae00" engName="Normal"
-                paraPrIDRef="0" charPrIDRef="0" nextStyleIDRef="0"
-                langID="1042" lockForm="0"/>
-    </hh:styles>
-  </hh:refList>
-</hh:head>"""
+def read_template_bytes(name: str) -> bytes:
+    return (TEMPLATES / name).read_bytes()
 
 
 def make_section(title: str, body: str, paper: str = "A4", columns: int = 1) -> str:
-    """섹션 XML 생성. paper: 'A4' | 'B4', columns: 1 | 2"""
     if paper == "B4":
-        page_pr = '<hp:pagePr landscape="WIDELY" width="72851" height="103181" gutterType="LEFT_ONLY">' \
-                  '<hp:margin header="4252" footer="4252" gutter="0" left="8504" right="8504" top="7086" bottom="5668"/>' \
-                  '</hp:pagePr>'
+        page_pr = ('<hp:pagePr landscape="WIDELY" width="72851" height="103181" gutterType="LEFT_ONLY">'
+                   '<hp:margin header="4252" footer="4252" gutter="0" left="8504" right="8504" top="7086" bottom="5668"/>'
+                   '</hp:pagePr>')
     else:
-        page_pr = '<hp:pagePr landscape="WIDELY" width="59528" height="84188" gutterType="LEFT_ONLY">' \
-                  '<hp:margin header="4252" footer="4252" gutter="0" left="8504" right="8504" top="5668" bottom="4252"/>' \
-                  '</hp:pagePr>'
+        page_pr = ('<hp:pagePr landscape="WIDELY" width="59528" height="84188" gutterType="LEFT_ONLY">'
+                   '<hp:margin header="4252" footer="4252" gutter="0" left="8504" right="8504" top="5668" bottom="4252"/>'
+                   '</hp:pagePr>')
 
     col_type = "NEWSPAPER" if columns > 1 else "NONE"
-
-    # XML escape
-    import html
     title_esc = html.escape(title)
     body_esc = html.escape(body)
 
     paragraphs = []
-    # Title
-    paragraphs.append(f'''  <hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">
-    <hp:run charPrIDRef="0"><hp:t>{title_esc}</hp:t></hp:run>
-  </hp:p>''')
-    # Empty line
-    paragraphs.append('''  <hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">
-    <hp:run charPrIDRef="0"><hp:t/></hp:run>
-  </hp:p>''')
-    # Body paragraphs
+    paragraphs.append(f'<hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">'
+                      f'<hp:run charPrIDRef="0"><hp:t>{title_esc}</hp:t></hp:run></hp:p>')
+    paragraphs.append('<hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">'
+                      '<hp:run charPrIDRef="0"><hp:t/></hp:run></hp:p>')
     for line in body_esc.split("\\n"):
-        if line.strip():
-            paragraphs.append(f'''  <hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">
-    <hp:run charPrIDRef="0"><hp:t>{line}</hp:t></hp:run>
-  </hp:p>''')
+        line = line.strip()
+        if line:
+            paragraphs.append(f'<hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">'
+                              f'<hp:run charPrIDRef="0"><hp:t>{line}</hp:t></hp:run></hp:p>')
 
-    body_xml = "\n".join(paragraphs)
+    body_xml = "".join(paragraphs)
 
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<hs:sec xmlns:ha="http://www.hancom.co.kr/hwpml/2011/app"
-        xmlns:hp="http://www.hancom.co.kr/hwpml/2011/paragraph"
-        xmlns:hs="http://www.hancom.co.kr/hwpml/2011/section"
-        xmlns:hc="http://www.hancom.co.kr/hwpml/2011/core"
-        xmlns:hh="http://www.hancom.co.kr/hwpml/2011/head">
-  <hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">
-    <hp:run charPrIDRef="0">
-      <hp:secPr id="" textDirection="HORIZONTAL" spaceColumns="1134" tabStop="8000" tabStopVal="4000" tabStopUnit="HWPUNIT" outlineShapeIDRef="0" memoShapeIDRef="0" textVerticalWidthHead="0" masterPageCnt="0">
-        <hp:grid lineGrid="0" charGrid="0" wonggojiFormat="0"/>
-        <hp:colPr id="" type="{col_type}" layout="LEFT" colCount="{columns}" sameSz="1" sameGap="2268"/>
-        <hp:startNum pageStartsOn="BOTH" page="0" pic="0" tbl="0" equation="0"/>
-        {page_pr}
-      </hp:secPr>
-    </hp:run>
-  </hp:p>
-{body_xml}
-</hs:sec>'''
+    return (f'<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'
+            f'<hs:sec xmlns:ha="http://www.hancom.co.kr/hwpml/2011/app" '
+            f'xmlns:hp="http://www.hancom.co.kr/hwpml/2011/paragraph" '
+            f'xmlns:hp10="http://www.hancom.co.kr/hwpml/2016/paragraph" '
+            f'xmlns:hs="http://www.hancom.co.kr/hwpml/2011/section" '
+            f'xmlns:hc="http://www.hancom.co.kr/hwpml/2011/core" '
+            f'xmlns:hh="http://www.hancom.co.kr/hwpml/2011/head" '
+            f'xmlns:hwpunitchar="http://www.hancom.co.kr/hwpml/2016/HwpUnitChar">'
+            f'<hp:p id="0" paraPrIDRef="0" styleIDRef="0" pageBreak="0" columnBreak="0" merged="0">'
+            f'<hp:run charPrIDRef="0">'
+            f'<hp:secPr id="" textDirection="HORIZONTAL" spaceColumns="1134" tabStop="8000" tabStopVal="4000" tabStopUnit="HWPUNIT" outlineShapeIDRef="0" memoShapeIDRef="0" textVerticalWidthHead="0" masterPageCnt="0">'
+            f'<hp:grid lineGrid="0" charGrid="0" wonggojiFormat="0"/>'
+            f'<hp:colPr id="" type="{col_type}" layout="LEFT" colCount="{columns}" sameSz="1" sameGap="2268"/>'
+            f'<hp:startNum pageStartsOn="BOTH" page="0" pic="0" tbl="0" equation="0"/>'
+            f'{page_pr}'
+            f'</hp:secPr>'
+            f'</hp:run></hp:p>'
+            f'{body_xml}'
+            f'</hs:sec>')
 
 
 def generate_hwpx(output_path: str, title: str, body: str,
                   paper: str = "A4", columns: int = 1):
-    """HWPX 파일 생성"""
-    # content.hpf — 작동하는 한글 파일과 동일한 네임스페이스 포함
-    content_hpf = '''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><opf:package xmlns:ha="http://www.hancom.co.kr/hwpml/2011/app" xmlns:hp="http://www.hancom.co.kr/hwpml/2011/paragraph" xmlns:hp10="http://www.hancom.co.kr/hwpml/2016/paragraph" xmlns:hs="http://www.hancom.co.kr/hwpml/2011/section" xmlns:hc="http://www.hancom.co.kr/hwpml/2011/core" xmlns:hh="http://www.hancom.co.kr/hwpml/2011/head" xmlns:hhs="http://www.hancom.co.kr/hwpml/2011/history" xmlns:hm="http://www.hancom.co.kr/hwpml/2011/master-page" xmlns:hpf="http://www.hancom.co.kr/schema/2011/hpf" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf/" xmlns:ooxmlchart="http://www.hancom.co.kr/hwpml/2016/ooxmlchart" xmlns:hwpunitchar="http://www.hancom.co.kr/hwpml/2016/HwpUnitChar" xmlns:epub="http://www.idpf.org/2007/ops" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" version="" unique-identifier="" id=""><opf:metadata><opf:title/><opf:language>ko</opf:language><opf:meta name="CreatedDate" content="text">2026-01-01T00:00:00Z</opf:meta></opf:metadata><opf:manifest><opf:item id="header" href="Contents/header.xml" media-type="application/xml"/><opf:item id="section0" href="Contents/section0.xml" media-type="application/xml"/><opf:item id="settings" href="settings.xml" media-type="application/xml"/></opf:manifest><opf:spine><opf:itemref idref="header" linear="yes"/><opf:itemref idref="section0" linear="yes"/></opf:spine></opf:package>'''
-
     with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zf:
-        # mimetype은 압축 없이
-        zf.writestr("mimetype", read_template("mimetype"), compress_type=zipfile.ZIP_STORED)
-        zf.writestr("version.xml", read_template("version.xml"))
-        zf.writestr("settings.xml", read_template("settings.xml"))
-        zf.writestr("META-INF/container.xml", read_template("container.xml"))
-        zf.writestr("META-INF/container.rdf", read_template("container.rdf"))
-        zf.writestr("META-INF/manifest.xml", read_template("manifest.xml"))
-        zf.writestr("Contents/content.hpf", content_hpf)
-        zf.writestr("Contents/header.xml", HEADER_XML)
+        zf.writestr("mimetype", "application/hwp+zip", compress_type=zipfile.ZIP_STORED)
+        zf.writestr("version.xml", read_template("version-real.xml"))
+        zf.writestr("settings.xml", read_template("settings-real.xml"))
+        zf.writestr("META-INF/container.xml", read_template("container-real.xml"))
+        zf.writestr("META-INF/container.rdf", read_template("container-real.rdf"))
+        zf.writestr("META-INF/manifest.xml", read_template("manifest-real.xml"))
+        zf.writestr("Contents/content.hpf", read_template("content-real.hpf"))
+        zf.writestr("Contents/header.xml", read_template("header-real.xml"))
         zf.writestr("Contents/section0.xml", make_section(title, body, paper, columns))
         zf.writestr("Preview/PrvText.txt", title)
 
@@ -156,10 +86,12 @@ def generate_hwpx(output_path: str, title: str, body: str,
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("사용법: python generator.py output.hwpx '제목' '본문'")
+        print("사용법: python generator.py output.hwpx '제목' '본문' [A4|B4] [1|2]")
         sys.exit(1)
 
     out = sys.argv[1]
     t = sys.argv[2]
     b = sys.argv[3] if len(sys.argv) > 3 else ""
-    generate_hwpx(out, t, b)
+    p = sys.argv[4] if len(sys.argv) > 4 else "A4"
+    c = int(sys.argv[5]) if len(sys.argv) > 5 else 1
+    generate_hwpx(out, t, b, p, c)
